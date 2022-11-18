6d4c3a8adc469ca959d478e927a341ef7ba80c5c
JamesNolan <vip17jamesnolan@gmail.com>
2022-11-19 01:14:52 +0800

    clean repo db and issue db

--- code/issue_cross_check.py
+++ code/issue_cross_check.py
@@ -1,22 +1,27 @@
+import collections
+
 import pandas as pd
 
 
 df_repo = pd.read_csv('repoDB.csv')
-df_issue = pd.read_csv('issueDB.csv').drop_duplicates()
-df_issue.to_csv('issueDB.csv', index=False)
+df_issue = pd.read_csv('issueDB.csv')
+
+#for index, paper in df_repo.iterrows():
+    #if paper['Name_Repo'] == 'PaddlePaddle/PaddleOCR':
+        #issue_string = str(paper['IDList_Issue'])
+        #issue_list = list(set([int(i) for i in issue_string.split('#')]))
+        #paper['IDList_Issue'] = "#".join([str(i) for i in issue_list])
+        #break
+
+#df_repo.to_csv('repoDB.csv', index=False)
+
+
+
+
 exit(0)
-
-
-
-
-
-
-
-
 
 print(len(df_issue))
 for index, paper in df_repo.iterrows():
-    #print(f'[{index + 1}] {paper["Name_Repo"]}')
     repo_name = paper['Name_Repo']
     issue_string = str(paper['IDList_Issue'])
     if issue_string == 'nan':
@@ -30,5 +35,6 @@ for index, paper in df_repo.iterrows():
         print(repo_name)
         print(f'Issue list recorded in repoDB: {len(issue_list)}-{issue_list}')
         print(f'Issue list recorded in issueDB: {len(issue_list_real)}-{issue_list_real}')
-        if repo_name == "khetan2/MBEM":
-            df_issue.query(f"`Name_Repo` == '{repo_name}'").to_csv('issue51.csv')
+        occured_list = []
+
+
