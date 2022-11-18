5c18b4a0c21cbb887dae79683d53a0b960f3ea8a
JamesNolan <vip17jamesnolan@gmail.com>
2022-11-19 01:26:32 +0800

    the DBs are all clean and correct

--- code/issue_cross_check.py
+++ code/issue_cross_check.py
@@ -6,19 +6,6 @@ import pandas as pd
 df_repo = pd.read_csv('repoDB.csv')
 df_issue = pd.read_csv('issueDB.csv')
 
-#for index, paper in df_repo.iterrows():
-    #if paper['Name_Repo'] == 'PaddlePaddle/PaddleOCR':
-        #issue_string = str(paper['IDList_Issue'])
-        #issue_list = list(set([int(i) for i in issue_string.split('#')]))
-        #paper['IDList_Issue'] = "#".join([str(i) for i in issue_list])
-        #break
-
-#df_repo.to_csv('repoDB.csv', index=False)
-
-
-
-
-exit(0)
 
 print(len(df_issue))
 for index, paper in df_repo.iterrows():
@@ -30,11 +17,15 @@ for index, paper in df_repo.iterrows():
         issue_list = [int(i) for i in issue_string.split('#')]
     #print(issue_list)
     issue_df = df_issue.query(f"`Name_Repo` == '{repo_name}'")['Identity_Repo']
-    issue_list_real = issue_df.tolist()
+    issue_list_real = [int(i) for i in issue_df.tolist()]
     if issue_list_real != issue_list:
         print(repo_name)
         print(f'Issue list recorded in repoDB: {len(issue_list)}-{issue_list}')
         print(f'Issue list recorded in issueDB: {len(issue_list_real)}-{issue_list_real}')
-        occured_list = []
+        issue_list_real.sort()
+        issue_list.sort()
+        for i in range(len(issue_list)):
+            if issue_list[i] != issue_list_real[i]:
+                print(f'Index {i}: {issue_list[i]}-{issue_list_real[i]}')
 
 
